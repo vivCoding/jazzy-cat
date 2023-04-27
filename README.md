20e2a0284720af3ad2cf95f3d646864442219c55
vivCoding <vivCoding@gmail.com>
2023-04-27 16:38:04 -0400

    jazz

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # a jazzy cat
 
 a discord chatbot/chatcat with vicuna weights
+
+### environment variables
+
+| Name            | Description                |
+| --------------- | -------------------------- |
+| `DISCORD_TOKEN` | your discord bot api token |
--- bot.py
+++ bot.py
@@ -4,22 +4,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class JazzyBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     async def on_ready(self):
-        print(f'{self.user} has connected to discord bois')
-        # send msg to first text channel
-        for guild in self.guilds:
-            for channel in guild.channels:
-                if channel.type == discord.ChannelType.text:
-                    await channel.send('the jazzy cat is here bois')
-                    break
+        print(f"{self.user} has connected to da discord bois")
+        # get all text channels it has access to
+        channels = [
+            channel
+            for channel in self.get_all_channels()
+            if channel.type == discord.ChannelType.text
+            and channel.permissions_for(channel.guild.me).send_messages
+        ]
+        # sort by visual position
+        channels.sort(key=lambda c: c.position)
+        # send message to first channel it has access to
+        await channels[0].send("the jazzy cat is here bois")
 
     async def on_message(self, message: discord.Message):
-        if message.author == self.user: return
+        # don't respond to itself
+        if message.author == self.user:
+            return
         await message.channel.send("im a wip, so i schleep")
+
 
 if __name__ == "__main__":
     intents = discord.Intents.default()
